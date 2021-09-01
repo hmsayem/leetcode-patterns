19bb34c5c0e48334631811b894e4ff2851816f49
hmsayem <hmsayem@appscode.com>
2021-09-02 00:07:42 +0600

    Add MissingNumber.cpp

---
+++ MissingNumber.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            res = res ^ i;
+            res = res ^ nums[i];
+        }
+        res = res ^ nums.size();
+        return res;
+    }
+};
